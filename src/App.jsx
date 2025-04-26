ae80ed340ca4d1a0e46d57e0dd0e457f5c78d48e
phatommobb <nickoluoch1130@gmail.com>
2025-04-26 15:45:18 +0300

    main code

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,7 @@ import { useReducer } from 'react';
 import AddTask from './AddTask';
 import './App.css';
 import TaskList from './TaskList';
-
-function taskReducer(tasks, action) {
-  if (action.type === 'added') {
-    return [...tasks, { id: action.id, text: action.text, done: false }];
-  } else if (action.type === 'changed') {
-    return tasks.map((t) => {
-      if (t.id === action.task.id) {
-        return action.task;
-      } else {
-        return t;
-      }
-    });
-  } else if (action.type === 'deleted') {
-    return tasks.filter((t) => t.id !== action.taskId);
-  }
-}
+import taskReducer from './taskReducer';
 
 function App() {
   const [tasks, dispatch] = useReducer(taskReducer, initialTasks);
