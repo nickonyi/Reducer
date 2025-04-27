c5f476e1a00de0c93f259fdb44275e94095d18cc
phatommobb <nickoluoch1130@gmail.com>
2025-04-27 20:34:57 +0300

    just code

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ let nextId = 3;
 const initialTasks = [
   { id: 0, text: 'Visit Kafka Museum', done: true },
   { id: 1, text: 'Watch a puppet show', done: false },
-  { id: 2, text: 'Lennon Wall pic', done: false },
+  { id: 2, text: 'Lennon Wall pic', done: true },
 ];
 
 export default App;
