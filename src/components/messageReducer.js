b14638d0c635ee0cdd51653ca1a483621dc367e5
phatommobb <nickoluoch1130@gmail.com>
2025-04-29 20:00:14 +0300

    reducer code

--- src/components/messageReducer.js
+++ src/components/messageReducer.js
@@ -5,7 +5,14 @@ export const initialState = {
 
 export function messageReducer(state, action) {
   switch (action.type) {
-    case 'changed_selection':
+    case 'changed_selection': {
       return { ...state, selectedId: action.contactId, message: '' };
+    }
+    case 'edited_message': {
+      return { ...state, message: action.message };
+    }
+    case 'sent_message': {
+      return { ...state, message: '' };
+    }
   }
 }
