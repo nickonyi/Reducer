ebe319ba8761cce844b73b2b4628aedbf45c8375
phatommobb <nickoluoch1130@gmail.com>
2025-04-29 18:45:36 +0300

    main code

---
+++ src/components/Messenger.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Messenger() {
+  return <div>Messenger</div>;
+}
+
+export default Messenger;
+
+const contacts = [
+  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
+  { id: 1, name: 'Alice', email: 'alice@mail.com' },
+  { id: 2, name: 'Bob', email: 'bob@mail.com' },
+];
