0b7a5e56cda261df16ec26d96eca0768acba3d15
phatommobb <nickoluoch1130@gmail.com>
2025-04-29 19:07:51 +0300

    main code

--- src/components/Messenger.jsx
+++ src/components/Messenger.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { messageReducer, initialState } from './messageReducer';
+import ContactList from './ContactList';
+import Chat from './Chat';
+import { contacts } from './contact';
 
 function Messenger() {
-  return <div>Messenger</div>;
+  const [state, dispatch] = useReducer(messageReducer, initialState);
+  const message = state.messages[state.selectId];
+  const contact = contacts.find((c) => c.id === state.selectId);
+
+  return (
+    <div>
+      <ContactList
+        contacts={contacts}
+        selectId={state.selectId}
+        dispatch={dispatch}
+        message={message}
+      />
+      <Chat
+        key={contact.id}
+        message={message}
+        contact={contact}
+        dispatch={dispatch}
+      />
+    </div>
+  );
 }
 
 export default Messenger;
-
-const contacts = [
-  { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
-  { id: 1, name: 'Alice', email: 'alice@mail.com' },
-  { id: 2, name: 'Bob', email: 'bob@mail.com' },
-];
---
+++ src/components/messageReducer.js
@@ -0,0 +1,10 @@
+export const initialState = {
+  selectedId: 0,
+  messages: {
+    0: 'Hello, Taylor',
+    1: 'Hello, Alice',
+    2: 'Hello, Bob',
+  },
+};
+
+export function messageReducer(state, action) {}
